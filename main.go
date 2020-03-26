caad895aca821fbbecda5dc91b32c46a4570be8d
Adam Scholey <hello@adamjs.net>
2020-03-26 08:42:52 +0000

    Parse failed response data from temporary cache

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+const asdaStatusUnavailable = "UNAVAILABLE"
+
+type asdaResponse struct {
+	StatusCode string `json:"statusCode"`
+}
+
 func main() {
-	
+	contents := getFailDataFromCache()
+	var response asdaResponse
+
+	err := json.Unmarshal(contents, &response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if response.StatusCode == asdaStatusUnavailable {
+		log.Fatal("site is down")
+	}
+
+	log.Printf("%+v", response)
+}
+
+func getFailDataFromCache() []byte {
+	data, err := ioutil.ReadFile("./data/cached_fail.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data
 }
