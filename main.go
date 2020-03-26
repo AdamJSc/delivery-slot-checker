75487754e532c087d77e48fba4e25d04f11bce5d
Adam Scholey <hello@adamjs.net>
2020-03-26 19:48:02 +0000

    Parse successful response data and identify available slots

--- main.go
+++ main.go
@@ -2,18 +2,37 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"time"
 )
 
+const asdaStatusAvailable = "AVAILABLE"
 const asdaStatusUnavailable = "UNAVAILABLE"
+
+type asdaDeliverySlot struct {
+	Status string `json:"status"`
+	StartTime time.Time `json:"start_time"`
+}
+
+func (ds asdaDeliverySlot) IsAvailable() bool {
+	return ds.Status == asdaStatusAvailable
+}
 
 type asdaResponse struct {
 	StatusCode string `json:"statusCode"`
+	Data struct {
+		SlotDays []struct {
+			Slots []struct {
+				SlotInfo asdaDeliverySlot `json:"slot_info"`
+			} `json:"slots"`
+		} `json:"slot_days"`
+	} `json:"data"`
 }
 
 func main() {
-	contents := getFailDataFromCache()
+	contents := getDataFromCache("cached_success")
 	var response asdaResponse
 
 	err := json.Unmarshal(contents, &response)
@@ -25,11 +44,20 @@ func main() {
 		log.Fatal("site is down")
 	}
 
-	log.Printf("%+v", response)
+	var availableSlots []asdaDeliverySlot
+	for _, slotDay := range response.Data.SlotDays {
+		for _, slot := range slotDay.Slots {
+			if slot.SlotInfo.IsAvailable() {
+				availableSlots = append(availableSlots, slot.SlotInfo)
+			}
+		}
+	}
+
+	log.Printf("%+v\nFound %d available slots", availableSlots, len(availableSlots))
 }
 
-func getFailDataFromCache() []byte {
-	data, err := ioutil.ReadFile("./data/cached_fail.txt")
+func getDataFromCache(filename string) []byte {
+	data, err := ioutil.ReadFile(fmt.Sprintf("./data/%s.txt", filename))
 	if err != nil {
 		log.Fatal(err)
 	}
