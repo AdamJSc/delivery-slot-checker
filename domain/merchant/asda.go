1558c7c03c5e48b19245241288834301fbc805a7
Benjamin Cable <cable.benjamin@protonmail.com>
2020-04-03 13:18:13 +0100

    cleanup asda merchant code

--- domain/merchant/asda.go
+++ domain/merchant/asda.go
@@ -15,15 +15,18 @@ const (
 	asdaStatusUnavailable = "UNAVAILABLE"
 )
 
+// AsdaClient defines a client for interacting with the Asda supermarket.
 type AsdaClient struct {
 	Client
 	URL string
 }
 
+// GetName returns the supermarket name.
 func (c AsdaClient) GetName() string {
 	return "Asda"
 }
 
+// GetDeliverySlots returns delivery slots from Asda within the given date range, for a given postcode.
 func (c AsdaClient) GetDeliverySlots(postcode string, from, to time.Time) ([]DeliverySlot, error) {
 	var response asdaAPIResponse
 
@@ -51,7 +54,9 @@ func (c AsdaClient) GetDeliverySlots(postcode string, from, to time.Time) ([]Del
 	}
 
 	if response.StatusCode == asdaStatusUnavailable {
-		return []DeliverySlot{}, apperrors.OfflineError(c.GetName())
+		return []DeliverySlot{}, apperrors.OfflineError{
+			Merchant: c.GetName(),
+		}
 	}
 
 	var slots []DeliverySlot
@@ -64,16 +69,19 @@ func (c AsdaClient) GetDeliverySlots(postcode string, from, to time.Time) ([]Del
 	return slots, nil
 }
 
+// AsdaDeliverySlot represents a delivery slot for Asda.
 type AsdaDeliverySlot struct {
 	DeliverySlot
 	Status    string    `json:"status"`
 	StartTime time.Time `json:"start_time"`
 }
 
+// IsAvailable checks whether a delivery slot can still be booked.
 func (ds AsdaDeliverySlot) IsAvailable() bool {
 	return ds.Status == asdaStatusAvailable
 }
 
+// GetTime returns the start time of the delivery slot
 func (ds AsdaDeliverySlot) GetTime() time.Time {
 	return ds.StartTime
 }
@@ -90,33 +98,56 @@ type asdaAPIResponse struct {
 }
 
 type asdaAPIRequest struct {
-	Data struct {
-		ServiceInfo struct {
-			FulfillmentType string `json:"fulfillment_type"`
-		} `json:"service_info"`
-		StartDate      time.Time `json:"start_date"`
-		EndDate        time.Time `json:"end_date"`
-		ServiceAddress struct {
-			Postcode string `json:"postcode"`
-		} `json:"service_address"`
-		CustomerInfo struct {
-			AccountID string `json:"account_id"`
-		} `json:"customer_info"`
-		OrderInfo struct {
-			OrderID string `json:"order_id"`
-		} `json:"order_info"`
-	} `json:"data"`
+	Data Data `json:"data"`
+}
+
+// Data defines the data within an asdaAPIrequest.
+type Data struct {
+	ServiceInfo    ServiceInfo    `json:"service_info"`
+	ServiceAddress ServiceAddress `json:"service_address"`
+	CustomerInfo   CustomerInfo   `json:"customer_info"`
+	OrderInfo      OrderInfo      `json:"order_info"`
+	StartDate      time.Time      `json:"start_date"`
+	EndDate        time.Time      `json:"end_date"`
+}
+
+// ServiceInfo defines the service provided by asda.
+type ServiceInfo struct {
+	FulfillmentType string `json:"fulfillment_type"`
+}
+
+// ServiceAddress holds the postcode requested.
+type ServiceAddress struct {
+	Postcode string `json:"postcode"`
+}
+
+// CustomerInfo holds account information for the customer.
+type CustomerInfo struct {
+	AccountID string `json:"account_id"`
+}
+
+// OrderInfo holds order information relevant to the request.
+type OrderInfo struct {
+	OrderID string `json:"order_id"`
 }
 
 func newAsdaAPIRequest(postcode string, start, end time.Time) asdaAPIRequest {
-	var request asdaAPIRequest
-
-	request.Data.ServiceInfo.FulfillmentType = "DELIVERY"
-	request.Data.StartDate = start
-	request.Data.EndDate = end
-	request.Data.ServiceAddress.Postcode = postcode
-	request.Data.CustomerInfo.AccountID = "0"
-	request.Data.OrderInfo.OrderID = "0"
-
-	return request
+	return asdaAPIRequest{
+		Data: Data{
+			ServiceInfo: ServiceInfo{
+				FulfillmentType: "DELIVERY",
+			},
+			ServiceAddress: ServiceAddress{
+				Postcode: postcode,
+			},
+			CustomerInfo: CustomerInfo{
+				AccountID: "0",
+			},
+			OrderInfo: OrderInfo{
+				OrderID: "0",
+			},
+			StartDate: start,
+			EndDate:   end,
+		},
+	}
 }
